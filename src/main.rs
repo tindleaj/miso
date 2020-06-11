7421af330f80699ba973abe8a4a7c90c8decfe44
Austin Tindle <tindleaj@protonmail.com>
2020-06-10 18:40:54 -0600

    Multiple sounds playing at once

--- src/main.rs
+++ src/main.rs
@@ -2,31 +2,11 @@ extern crate iced;
 extern crate rodio;
 
 use iced::{Checkbox, Column, Container, Element, Length, Sandbox, Settings};
+use rodio::{Decoder, Sink, Source};
+use std::fs::File;
 use std::io::BufReader;
-use std::thread::JoinHandle;
-use std::time::Duration;
-
-// fn main() {
-//     let device = rodio::default_output_device().unwrap();
-
-//     let file = std::fs::File::open("birds.wav").unwrap();
-//     let play = rodio::play_once(&device, BufReader::new(file)).unwrap();
-//     play.set_volume(1.0);
-//     println!("Started play");
-
-//     thread::sleep(Duration::from_millis(15000));
-// }
-
-fn play_audio() -> JoinHandle<()> {
-    std::thread::spawn(|| loop {
-        println!("Started play");
-        let device = rodio::default_output_device().unwrap();
-        let file = std::fs::File::open("birds.wav").unwrap();
-        let play = rodio::play_once(&device, BufReader::new(file)).unwrap();
-        play.set_volume(1.0);
-        play.sleep_until_end();
-    })
-}
+use std::sync::mpsc::{channel, Sender};
+use std::thread;
 
 pub fn main() {
     Miso::run(Settings::default())
@@ -34,47 +14,107 @@ pub fn main() {
 
 #[derive(Debug, Clone)]
 enum Message {
-    SoundToggled(bool),
+    BirdsToggled(bool),
+    OtherToggled(bool),
 }
 
-#[derive(Default)]
 struct Miso {
-    toggle: bool,
+    bird_worker: Option<Sender<bool>>,
+    other_worker: Option<Sender<bool>>,
 }
 
 impl Sandbox for Miso {
     type Message = Message;
 
     fn new() -> Miso {
-        Miso::default()
+        Miso {
+            bird_worker: None,
+            other_worker: None,
+        }
     }
 
     fn title(&self) -> String {
-        String::from("Miph")
+        String::from("Miso")
     }
 
     fn update(&mut self, message: Self::Message) {
         match message {
-            Message::SoundToggled(true) => {
-                dbg!("play");
-                self.toggle = true;
-                play_audio();
+            Message::BirdsToggled(val) => {
+                if val == true {
+                    let device = rodio::default_output_device().unwrap();
+                    let sink = Sink::new(&device);
+                    let file = File::open("birds.wav").unwrap();
+                    let source = Decoder::new(BufReader::new(file)).unwrap();
+
+                    let (tx, rx) = channel();
+                    thread::spawn(move || {
+                        sink.append(source.repeat_infinite());
+
+                        loop {
+                            let msg = rx.recv().unwrap();
+                            if msg == false {
+                                break;
+                            }
+                        }
+                    });
+                    self.bird_worker = Some(tx);
+                } else {
+                    match &self.bird_worker {
+                        Some(sender) => {
+                            let res = sender.send(false);
+                            self.bird_worker = None;
+                        }
+                        None => unimplemented!(),
+                    }
+                }
             }
-            Message::SoundToggled(false) => {
-                dbg!("pause");
-                self.toggle = false
+            Message::OtherToggled(val) => {
+                if val == true {
+                    let device = rodio::default_output_device().unwrap();
+                    let sink = Sink::new(&device);
+                    let file = File::open("waves.wav").unwrap();
+                    let source = Decoder::new(BufReader::new(file)).unwrap();
+
+                    let (tx, rx) = channel();
+                    thread::spawn(move || {
+                        sink.append(source.repeat_infinite());
+
+                        loop {
+                            let msg = rx.recv().unwrap();
+                            if msg == false {
+                                break;
+                            }
+                        }
+                    });
+                    self.other_worker = Some(tx);
+                } else {
+                    match &self.other_worker {
+                        Some(sender) => {
+                            let res = sender.send(false);
+                            self.other_worker = None;
+                        }
+                        None => unimplemented!(),
+                    }
+                }
             }
         }
     }
 
     fn view(&mut self) -> Element<Self::Message> {
-        let checkbox = Checkbox::new(self.toggle, "Toggle", Message::SoundToggled);
+        let checkbox = Checkbox::new(self.bird_worker.is_some(), "Birds", |state| {
+            Message::BirdsToggled(state)
+        });
+
+        let other_checkbox = Checkbox::new(self.other_worker.is_some(), "Waves", |state| {
+            Message::OtherToggled(state)
+        });
 
         let content = Column::new()
             .spacing(20)
             .padding(20)
             .max_width(600)
-            .push(checkbox);
+            .push(checkbox)
+            .push(other_checkbox);
 
         Container::new(content)
             .width(Length::Fill)
